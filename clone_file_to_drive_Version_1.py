1e2c08f5e4d4f705ff7410d245855c1e43c23a6b
Patryk Szczepanik <skowik9@gmail.com>
2024-04-09 19:17:18 +0200

    optimalization

---
+++ clone_file_to_drive_Version_1.py
@@ -0,0 +1,67 @@
+# Author: Emsii 
+# Date: 20.03.2024
+# https://github.com/EmsiiDiss
+#
+# A program designed for copying folders and files from one location to another, 
+# specifically targeting folders containing G-code files. 
+# This tool is essential for private applications.
+
+import datetime
+import os
+import shutil
+import threading
+
+drive_dest = "E:/"
+drive_src = "G:/.shortcut-targets-by-id/1NV5-iDSWkzujlti1hy-S4DMccnFwygvG/DYSK/"
+mypath_list = ["Druk_3D", "Projekty_Inne"]
+
+liter = 0
+sum = 0
+f = []
+
+def list_folders(mypath):
+    for (dirpath, dirnames, filenames) in os.walk(drive_src + mypath):
+        f.extend(dirnames)
+        break
+    f.remove("Wzór")
+    f.sort()
+    return f
+
+def copy_file(mypath, dirnames):
+    global sum
+    src = drive_src + mypath + "/" + dirnames + "/G-Code"
+    dest = drive_dest + mypath + "/" + dirnames
+    
+    
+    if (not os.path.exists(dest)) or (os.stat(src).st_mtime - os.stat(dest).st_mtime > 1) or :
+        shutil.copytree(src, dest, ignore=shutil.ignore_patterns('*.ini', '.tmp*'), dirs_exist_ok=True, copy_function=shutil.copy2)
+        string_in = str(round(sum/len(f)*100 + liter, 2)) + "%"
+        sum += 1
+        print(f"{string_in.ljust(10)} | Folder {dirnames} copied!")
+
+def copy():
+    threads = []
+    for mypath in mypath_list:
+        f = list_folders(mypath)
+        for dirnames in f:
+            if os.path.exists(drive_src + mypath + "/" + dirnames + "/G-Code"):
+                t1 = threading.Thread(target=copy_file, args=(mypath, dirnames,))
+                t1.start()
+                threads.append(t1)
+
+    # Joining all threads to wait for their completion
+    for thread in threads:
+        thread.join()
+
+try:
+    now = datetime.datetime.now()
+    copy()
+
+except KeyboardInterrupt:
+    print("Break")
+
+finally:
+    print(f"{"100%".ljust(10)} | All folders copied!")
+    now = abs(datetime.datetime.now() - now)
+    now = now.seconds
+    print(f"Czas kopiowania: {now} sekund!")
---
+++ clone_file_to_drive_Version_2.py
@@ -0,0 +1,61 @@
+# Author: Emsii 
+# Date: 20.03.2024
+# https://github.com/EmsiiDiss
+#
+# A program designed for copying folders and files from one location to another, 
+# specifically targeting folders containing G-code files. 
+# This tool is essential for private applications.
+
+import datetime
+import os, shutil
+from pathlib import Path
+from humanize import naturalsize
+
+drive_dest = "E:/"
+drive_src = "G:/.shortcut-targets-by-id/1NV5-iDSWkzujlti1hy-S4DMccnFwygvG/DYSK/"
+mypath_list = ["Druk_3D","Projekty_Inne"]
+
+
+def get_size(path):
+    size = 0
+    for file_ in Path(path).rglob('*'):
+        size += file_.stat().st_size
+    return naturalsize(size)
+
+def copy():
+    liter = 0
+    sum = 0
+    f = []
+    for mypath in mypath_list:
+        for (dirpath, dirnames, filenames) in os.walk(drive_src + mypath):
+            f.extend(dirnames)
+            break
+
+        f.remove("Wzór")
+        f.sort()
+
+        for dirnames in f:
+            if os.path.exists(drive_src + mypath + "/" + dirnames + "/G-Code"):
+                src = drive_src + mypath + "/" + dirnames + "/G-Code"
+                dest = drive_dest + mypath + "/" + dirnames
+                liter = (f.index(dirnames)+1)/(len(f)+0.1)*100/len(mypath_list)
+                string_in = str(round(sum + liter, 2)) + "%"
+                if (not os.path.exists(dest)) or (os.stat(src).st_mtime - os.stat(dest).st_mtime > 1) or (get_size(src) != get_size(dest) and get_size(src) != "244 Bytes"):
+
+                    shutil.copytree(src, dest, ignore=shutil.ignore_patterns('*.ini', '.tmp*'), dirs_exist_ok=True, copy_function = shutil.copy2)
+                    print(f"{string_in.ljust(10)} | Folder {dirnames} copied!")
+        sum = liter
+        f = []
+
+try:
+    now = datetime.datetime.now()
+    print(f"{"0.00%".ljust(10)} | Starting copy files!")
+    copy()
+
+except KeyboardInterrupt:
+    print("Break")
+finally:
+    print(f"{"100%".ljust(10)} | All folders copied!")
+    now = abs(datetime.datetime.now() - now)
+    now = now.seconds
+    print(f"Czas kopiowania: {now} sekund!")    
