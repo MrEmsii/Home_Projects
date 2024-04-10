be1053ad75afe27c386cbf194a00d536766a7312
Patryk Szczepanik <skowik9@gmail.com>
2024-04-10 19:07:46 +0200

    add def removed empty folders

--- clone_file_to_drive_Version_2.py
+++ clone_file_to_drive_Version_2.py
@@ -7,55 +7,70 @@
 # This tool is essential for private applications.
 
 import datetime
-import os, shutil
-from pathlib import Path
-from humanize import naturalsize
+import os
+import shutil
+import threading
 
 drive_dest = "E:/"
 drive_src = "G:/.shortcut-targets-by-id/1NV5-iDSWkzujlti1hy-S4DMccnFwygvG/DYSK/"
-mypath_list = ["Druk_3D","Projekty_Inne"]
+mypath_list = ["Druk_3D", "Projekty_Inne"]
 
+liter = 0
+sum = 0
+f = []
 
-def get_size(path):
-    size = 0
-    for file_ in Path(path).rglob('*'):
-        size += file_.stat().st_size
-    return naturalsize(size)
+def list_folders(mypath):
+    for (dirpath, dirnames, filenames) in os.walk(drive_src + mypath):
+        f.extend(dirnames)
+        break
+    f.remove("Wzór")
+    f.sort()
+    return f
+
+def copy_file(mypath, dirnames):
+    global sum
+    src = drive_src + mypath + "/" + dirnames + "/G-Code"
+    dest = drive_dest + mypath + "/" + dirnames
+    
+    
+    if (not os.path.exists(dest)) or (os.stat(src).st_mtime - os.stat(dest).st_mtime > 1):
+        shutil.copytree(src, dest, ignore=shutil.ignore_patterns('*.ini', '.tmp*'), dirs_exist_ok=True, copy_function=shutil.copy2)
+        string_in = str(round(sum/len(f)*100 + liter, 2)) + "%"
+        sum += 1
+        print(f"{string_in.ljust(10)} | Folder {dirnames} copied!")
 
 def copy():
-    liter = 0
-    sum = 0
-    f = []
+    threads = []
     for mypath in mypath_list:
-        for (dirpath, dirnames, filenames) in os.walk(drive_src + mypath):
-            f.extend(dirnames)
-            break
-
-        f.remove("Wzór")
-        f.sort()
-
+        f = list_folders(mypath)
         for dirnames in f:
             if os.path.exists(drive_src + mypath + "/" + dirnames + "/G-Code"):
-                src = drive_src + mypath + "/" + dirnames + "/G-Code"
-                dest = drive_dest + mypath + "/" + dirnames
-                liter = (f.index(dirnames)+1)/(len(f)+0.1)*100/len(mypath_list)
-                string_in = str(round(sum + liter, 2)) + "%"
-                if (not os.path.exists(dest)) or (os.stat(src).st_mtime - os.stat(dest).st_mtime > 1) or (get_size(src) != get_size(dest) and get_size(src) != "244 Bytes"):
+                t1 = threading.Thread(target=copy_file, args=(mypath, dirnames,))
+                t1.start()
+                threads.append(t1)
 
-                    shutil.copytree(src, dest, ignore=shutil.ignore_patterns('*.ini', '.tmp*'), dirs_exist_ok=True, copy_function = shutil.copy2)
-                    print(f"{string_in.ljust(10)} | Folder {dirnames} copied!")
-        sum = liter
-        f = []
+    # Joining all threads to wait for their completion
+    for thread in threads:
+        thread.join()
+
+
+def delete_empty_folders(root = drive_dest):
+   for dirpath, dirnames, filenames in os.walk(root, topdown=False):
+      for dirname in dirnames:
+         full_path = os.path.join(dirpath, dirname)
+         if not os.listdir(full_path): 
+            os.rmdir(full_path)
 
 try:
     now = datetime.datetime.now()
-    print(f"{"0.00%".ljust(10)} | Starting copy files!")
     copy()
+    delete_empty_folders()
 
 except KeyboardInterrupt:
     print("Break")
+
 finally:
     print(f"{"100%".ljust(10)} | All folders copied!")
     now = abs(datetime.datetime.now() - now)
     now = now.seconds
-    print(f"Czas kopiowania: {now} sekund!")    
+    print(f"Czas kopiowania: {now} sekund!")
